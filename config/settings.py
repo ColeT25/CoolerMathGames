1a7a01e2cb428dda64d8a367df6f6c8ca2e12911
ColeT25 <116309785+ColeT25@users.noreply.github.com>
2023-03-21 17:55:38 -0400

    Connected cooler math games to a aws cloud based database

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CoolerMathGames',
+        'USER': 'ColeT',
+        'PASSWORD': 'coleThacker091602#',
+        'HOST': 'cooler-math-games.cctffrsopstu.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
