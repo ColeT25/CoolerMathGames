5e2505b9f706a9b9c8dcf5925818de19df03e26f
KhumboPhiri <102994495+KhumboPhiri@users.noreply.github.com>
2023-03-29 11:43:10 -0400

    Update views.py

--- cooler_math_games/views.py
+++ cooler_math_games/views.py
@@ -71,7 +71,7 @@ def mines(request):
 
 def catcher(request):
     # todo right now the user is always anon, will be changed in the future
-    catcher_game_db = Game.objects.get(name='mines')
+    catcher_game_db = Game.objects.get(name='catcher')
     catcher_game_db.total_plays += 1
     catcher_game_db.save()
     return render(request, 'cooler_math_games/catcher.html', {'user': 'anon'})
